2357c4174fcaf494a4a87ba3654eea64a2ff1265
Gindi Khangura <khangura_9@hotmail.com>
2019-06-13 21:51:10 -0500

    Implemented classes. All tests passing in Py2 and 3.

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,24 @@
 
+
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.posts = []
+        self.following = []
 
     def add_post(self, post):
-        pass
+        self.posts.append(post)
 
     def get_timeline(self):
-        pass
+        timeline = []
+        for following in self.following:
+            timeline.extend(following.posts)
+        return timeline
 
     def follow(self, other):
-        pass
+        self.following.append(other)
+
+    def __repr__(self):
+        return '{classname}: "{firstname} {lastname}"'.format(classname=self.__class__.__name__, firstname=self.first_name, lastname=self.last_name)
--- social_network/posts.py
+++ social_network/posts.py
@@ -1,36 +1,45 @@
 from datetime import datetime
 
-# Please remove the comments and 
-# create these classes as it corresponds:
-# (your tests will fail if you don't comment out these classes)
+class Post(object):
+    def __init__(self, text, timestamp=None):
+        self.text = text
+        if not timestamp:
+            timestamp = datetime.now()
+        self.timestamp = timestamp
+        self.user = None
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+    def set_user(self, user):
+        self.user = user
 
+    def get_formatted_timestamp(self):
+        return self.timestamp.strftime('%A, %b %d, %Y')
 
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+    def __repr__(self):
+        return '{classname}: {text}'.format(classname=self.__class__.__name__, text=self.text)
 
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class TextPost(Post):
+    def __init__(self, text, timestamp=None):
+        super(TextPost, self).__init__(text, timestamp)
+
+    def __str__(self):
+        return '@{firstname} {lastname}: "{text}"\n\t{timestamp}'.format(firstname=self.user.first_name, lastname=self.user.last_name, text=self.text, timestamp=self.get_formatted_timestamp())
 
 
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class PicturePost(Post):
+    def __init__(self, text, image_url, timestamp=None):
+        super(PicturePost, self).__init__(text, timestamp)
+        self.image_url = image_url
+
+    def __str__(self):
+        return '@{firstname} {lastname}: "{text}"\n\t{image_url}\n\t{timestamp}'.format(firstname=self.user.first_name, lastname=self.user.last_name, text=self.text, image_url=self.image_url, timestamp=self.get_formatted_timestamp())
+
+
+class CheckInPost(Post):
+    def __init__(self, text, latitude, longitude, timestamp=None):
+        super(CheckInPost, self).__init__(text, timestamp)
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def __str__(self):
+        return '@{firstname} Checked In: "{text}"\n\t{latitude}, {longitude}\n\t{timestamp}'.format(firstname=self.user.first_name, lastname=self.user.last_name, text=self.text, latitude=self.latitude, longitude=self.longitude, timestamp=self.get_formatted_timestamp())
